b3ffdca0a386ad0f68a62743b8d8509890bd14aa
b27676110 <105021036@live.asia.edu.tw>
2016-09-12 15:53:40 +0800

    hw00-1

---
+++ hw00-1/src/haha.java
@@ -0,0 +1,13 @@
+
+public class haha {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("我叫做鄭東欣^^資工系數位組 系草");
+
+	}
+
+}
